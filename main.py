7b74ac25cab62301aec238acca6a732569133f99
haimapi <haimapi@163.com>
2016-08-06 20:54:01 +0800

    create db

--- main.py
+++ main.py
@@ -1,15 +1,80 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import DevConfig
 
 
 app = Flask(__name__)
+app.config.from_object(DevConfig)
+db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return '<h1>hello</h1>'
 
-@app.route('/user/<name>')
-def user(name):
-    return 'hello, {}'.format(name)
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    posts = db.relationship(
+        'Post',
+        lazy='dynamic',
+        backref='user'
+    )
+
+    def __init__(self, username):
+        self.username = username
+
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    pub_date = db.Column(db.DateTime())
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    comments = db.relationship(
+        'Comment',
+        lazy='dynamic',
+        backref='post'
+    )
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.title)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime())
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.name)
+
+
+tags = db.Table('post_tags',
+    db.Column('post_id', db.Integer(), db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer(), db.ForeignKey('tag.id'))
+)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
